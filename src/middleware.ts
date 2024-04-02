cee812f9a14c1dcecad8e9991fb64e6cbb5aa80c
Joel Wekesa <joelwekesa@Joels-MacBook-Air-2.local>
2024-04-02 10:34:23 +0300

    Auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,9 @@
-import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
-import { NextResponse } from 'next/server';
+import { authMiddleware } from '@clerk/nextjs';
 
 export const config = {
     matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 }
 
 export default authMiddleware({
-
     publicRoutes: ["/register", "/contact", "/", "/sign-in", "/sign-up"],
-    debug: true,
-    afterAuth(auth, req, evt) {
-        return NextResponse.next();
-    }
 });
