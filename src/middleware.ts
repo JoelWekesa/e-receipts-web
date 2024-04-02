6d454f31a54faeb173b6cad36be4d1d8afabc486
Joel Wekesa <joelwekesa@Joels-MacBook-Air-2.local>
2024-04-02 11:35:11 +0300

    switch from bun to pnpm

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ export const config = {
 }
 
 export default authMiddleware({
-    publicRoutes: ["/register", "/contact", "/", "/sign-in", "/sign-up"],
+    publicRoutes: ["/register", "/contact", "/"],
 });
