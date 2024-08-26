fe749ea164c91e4252cba66b6adda0724ac5fbe0
Joel Wekesa <joelwekesa@ip-192-168-100-4.ec2.internal>
2024-08-27 00:48:51 +0300

    CICD test

--- src/utils/create-url.ts
+++ src/utils/create-url.ts
@@ -3,6 +3,5 @@ import { ReadonlyURLSearchParams } from "next/navigation";
 export const createUrl = (pathname: string, params: URLSearchParams | ReadonlyURLSearchParams) => {
     const paramsString = params.toString();
     const queryString = `${paramsString.length ? '?' : ''}${paramsString}`;
-
     return `${pathname}${queryString}`;
 };
